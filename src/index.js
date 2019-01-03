ee8ac328e9451472faeedf0c633b816ec2b23364
Prerak Rustagi <prerak1992@gmail.com>
2019-01-03 11:59:41 -0800

    Arrow function used for click handler.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={function() { alert('Clicked!'); }}>
+      <button className="square" onClick={() => alert('Clicked!') }>
         {this.props.value}
       </button>
     );
